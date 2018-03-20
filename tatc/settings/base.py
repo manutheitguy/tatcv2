63c06c1553a14ebbe89a07432536b94bbbc9bf19
Emmanuel Makonde <manutheitguy@gmail.com>
2018-03-20 11:24:09 +0300

    changing db

--- tatc/settings/base.py
+++ tatc/settings/base.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'tatc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -96,6 +96,20 @@ DATABASES = {
 #         'PORT': ''
 #     }
 # }
+
+# POSTGRES DATABASE SETTINGS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MYPASS',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
